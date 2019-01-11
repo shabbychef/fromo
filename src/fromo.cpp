0fd3b3f7fc9ed9fcce0ac248d5215372337ce059
Steven Pav <shabbychef@gmail.com>
2019-01-10 20:55:13 -0800

    documenting the problem of skewness denominator from #20
    towards zero ambiguity documentation, cf #21.

--- src/fromo.cpp
+++ src/fromo.cpp
@@ -127,6 +127,8 @@ using namespace Rcpp;
 //' if (require(moments)) {
 //'   skew4(x)[1] - skewness(x)
 //' }
+//'
+//'
 //' # check 'robustness'; only the mean should change:
 //' kurt5(x + 1e12) - kurt5(x)
 //' # check speed
@@ -140,6 +142,27 @@ using namespace Rcpp;
 //' y <- std_moments(x,6)
 //' cml <- cent_cumulants(x,6)
 //' std <- std_cumulants(x,6)
+//' 
+//' # check that skew matches moments::skewness
+//' if (require(moments)) {
+//'     set.seed(1234)
+//'     x <- rnorm(1000)
+//'     resu <- fromo::skew4(x)
+//' 
+//'     msku <- moments::skewness(x)
+//'     stopifnot(abs(msku - resu[1]) < 1e-14)
+//' }
+//' 
+//' # check skew vs e1071 skewness, which has a different denominator
+//' if (require(e1071)) {
+//'     set.seed(1234)
+//'     x <- rnorm(1000)
+//'     resu <- fromo::skew4(x)
+//' 
+//'     esku <- e1071::skewness(x,type=3)
+//'     nobs <- resu[4]
+//'     stopifnot(abs(esku - resu[1] * ((nobs-1)/nobs)^(3/2)) < 1e-14)
+//' }
 //'
 //' @template etc
 //' @template ref-romo
